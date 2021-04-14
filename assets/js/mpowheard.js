1152eab93781fe635ccb750a13285ce770352713
Melanie Panem <16231664+melanierosson@users.noreply.github.com>
2021-04-13 19:28:33 -0700

    quick work on modal js

--- assets/js/mpowheard.js
+++ assets/js/mpowheard.js
@@ -1,2 +1,18 @@
+// Footer copyright year variable for current year
 var year = new Date().getFullYear();
 document.getElementById('currentYear').innerText = year;
+
+
+// ------- Options Page
+const optModal = document.getElementById('modal-overlay');
+const optLinks = document.querySelectorAll('div.opt.row');
+
+for (var i = 0; i < optLinks.length; i++) {
+	console.log(i); // index
+	console.log(optLinks[i]); // value
+	optLinks[i].addEventListener('click', function() {
+		optModal.classList.add('enabled');
+		// add show content by index+option
+		// add click out or close button to hide modal
+	});
+}
